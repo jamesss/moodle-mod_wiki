38cebe2a1376701486a31603cd57d6d2688e0a17
James Simpson <js2310@doc.ic.ac.uk>
2011-09-11 13:48:46 +0100

    Initial commit

---
+++ version.php
@@ -0,0 +1,12 @@
+<?php // $Id: version.php,v 1.31 2010/05/28 09:49:34 pigui Exp $
+
+////////////////////////////////////////////////////////////////////////////////
+//  Code fragment to define the module version etc.
+//  This fragment is called by /admin/index.php
+////////////////////////////////////////////////////////////////////////////////
+
+$module->version  = 2010052800;
+$module->requires = 2007020200;  // Requires this Moodle version. 1.8 or newer
+$module->cron     = 0; // How often should cron check this module (seconds)?
+
+?>
